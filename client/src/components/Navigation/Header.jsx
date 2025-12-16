b8ac19bf2d5c5e44da8d3bed021c7e465f9325cb
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2025-12-16 19:27:03 +0200

    fix: added missing component

--- client/src/components/Navigation/Header.jsx
+++ client/src/components/Navigation/Header.jsx
@@ -196,7 +196,7 @@ function Header() {
               </svg>
             </button>
 
-            <button
+            {/* <button
               type="button"
               aria-label="Notifications"
               className="p-2 rounded-md bg-white/6 backdrop-blur-md border border-white/10 hover:bg-white/12 hover:shadow-[0_0_10px_rgba(79,195,247,0.12)] transition"
@@ -215,7 +215,7 @@ function Header() {
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />
               </svg>
-            </button>
+            </button> */}
           </div>
 
           {!user ? (
